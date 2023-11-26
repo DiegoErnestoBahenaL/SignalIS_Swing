036d714d5a4086eda6066e80a13ab0601849d069
Adrian Diaz <a22310379@ceti.mx>
2023-11-25 22:48:38 -0600

    Registro Curso y Registro Capacitacion

---
+++ src/Components/RegistroCapacitación.java
@@ -0,0 +1,23 @@
+package Components;
+
+import javax.swing.*;
+
+public class RegistroCapacitación {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JSpinner spinner1;
+    private JCheckBox opcion1CheckBox;
+    private JCheckBox opcion2CheckBox;
+    private JCheckBox opcion3CheckBox;
+    private JCheckBox opcion1CheckBox1;
+    private JCheckBox opcion2CheckBox1;
+    private JCheckBox opcion3CheckBox1;
+    private JTextArea textArea1;
+    private JButton regresarButton;
+    private JButton registrarButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/Components/RegistroCurso.java
@@ -0,0 +1,13 @@
+package Components;
+
+import javax.swing.*;
+
+public class RegistroCurso {
+    private JPanel panel1;
+    private JLabel RegistrarCurso;
+    private JComboBox comboBox1;
+    private JSpinner spinner1;
+    private JTextArea textArea1;
+    private JButton regresarButton;
+    private JButton registrarButton;
+}
