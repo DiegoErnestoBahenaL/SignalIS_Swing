36c136e376b40e90569d73fe0e29b2de1c4efc7a
Adrian Diaz <a22310379@ceti.mx>
2023-11-25 23:10:06 -0600

    Registro de Certificado

---
+++ src/Components/RegistroCertificado.java
@@ -0,0 +1,13 @@
+package Components;
+
+import javax.swing.*;
+
+public class RegistroCertificado {
+    private JPanel panel1;
+    private JButton regresarButton;
+    private JButton registrarButton;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField textField1;
+}
