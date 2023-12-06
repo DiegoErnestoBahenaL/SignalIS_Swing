b311605652aa35dece53a53a983fb77ae50a7fa9
Estela386 <estelabarajaslozano@gmail.com>
2023-12-06 09:46:50 -0600

    Certificate

--- src/Components/Controllers/ShowCertificate.java
+++ src/Components/Controllers/ShowCertificate.java
@@ -1,7 +1,14 @@
 package Components.Controllers;
 
+import Data.DataStructures.List;
+import Data.Models.Certificate;
+import Data.Models.Course;
+import Data.Models.User;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ShowCertificate extends JInternalFrame{
     private JButton regresarButton;
@@ -9,18 +16,23 @@ public class ShowCertificate extends JInternalFrame{
     private JButton editarButton;
     private JButton siguienteButton;
     private JButton anteriorButton;
-    private JComboBox comboBox1;
-    private JTextField textField1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
     private JPanel panel1;
     private JLabel sub1;
     private JLabel sub2;
     private JLabel sub3;
-    private JLabel sub4;
+    private JLabel lbFecha;
     private JLabel titulo;
+    private JTextField tfCapacitacion;
+    private JTextField tfNombre;
+    private JTextField tfMaestro;
+    private JTextField tfFecha;
 
+    private long currentId = 0;
+    private int currentIndex = 0;
     public ShowCertificate(Landing landing){
+
+        ShowCertificate showCertificate = this;
+
         this.setSize(500, 300);
         this.setContentPane(panel1);
         this.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
@@ -36,9 +48,86 @@ public class ShowCertificate extends JInternalFrame{
         this.sub1.setFont(subtitle_font);
         this.sub2.setFont(subtitle_font);
         this.sub3.setFont(subtitle_font);
-        this.sub4.setFont(subtitle_font);
+        this.lbFecha.setFont(subtitle_font);
+
+        Certificate currentCertificate = null;
+        try {
+            currentCertificate = landing.Certificates.getItemAtIndex(currentIndex);
+
+            currentId = currentCertificate.getId();
+            // Mostrar el curso en la interfaz gráfica, por ejemplo, en un JTextArea o JLabel
+            tfNombre.setText(currentCertificate.getStudentName());
+            tfMaestro.setText(currentCertificate.getTeacherName());
+            tfCapacitacion.setText(String.valueOf(currentCertificate.getTrainingId()));
+            tfFecha.setText(currentCertificate.getDate());
+
+        } catch (Exception ex) {
+            ex.printStackTrace(); // Manejo de excepciones
+        }
+        this.setVisible(true);
+
+        siguienteButton.addActionListener(e -> {
+            if (currentIndex < landing.Courses.Count - 1) {
+                currentIndex++;
+                Certificate updatedCertificate = null;
+                try {
+                    updatedCertificate = landing.Certificates.getItemAtIndex(currentIndex);
+                    tfNombre.setText(updatedCertificate.getStudentName());
+                    tfMaestro.setText(updatedCertificate.getTeacherName());
+                    tfCapacitacion.setText(String.valueOf(updatedCertificate.getTrainingId()));
+                    tfFecha.setText(updatedCertificate.getDate());
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                JOptionPane.showMessageDialog(showCertificate, "Ya estás en el último curso");
+            }
+        });
+
+        anteriorButton.addActionListener(e -> {
+            if (currentIndex > 0) {
+                currentIndex--;
+                // Mostrar el curso anterior
+                Certificate previousCertificate = null;
+                try {
+                    tfNombre.setText(previousCertificate.getStudentName());
+                    tfMaestro.setText(previousCertificate.getTeacherName());
+                    tfCapacitacion.setText(String.valueOf(previousCertificate.getTrainingId()));
+                    tfFecha.setText(previousCertificate.getDate());
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                // Estás en el primer curso, maneja esto
+                JOptionPane.showMessageDialog(showCertificate, "Ya estás en el primer curso");
+            }
+        });
 
 
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showCertificate.dispose();
+            }
+        });
+
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Certificate certificateToDelete = landing.Certificates.getItemAtIndex(currentIndex);
+
+                    landing.Certificates.delete(certificateToDelete);
+
+                    JOptionPane.showMessageDialog(showCertificate, "Curso Eliminado");
+                }
+                catch ( Exception ex){
+
+                }
+            }
+        });
 
     }
 }
